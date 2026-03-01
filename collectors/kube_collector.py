1e2783f3c12b486f5c40853dc3b4ff107cbf77b9
prabha332 <prabhavatiagre@gmail.com>
2026-03-01 16:48:13 +0530

    Add function to retrieve failed Kubernetes pods

--- collectors/kube_collector.py
+++ collectors/kube_collector.py
@@ -1,1 +1,21 @@
+from kubernetes import client, config
 
+def get_failed_pods():
+    config.load_kube_config()
+    v1 = client.CoreV1Api()
+
+    failed = []
+
+    pods = v1.list_pod_for_all_namespaces()
+
+    for pod in pods.items:
+        phase = pod.status.phase
+
+        if phase != "Running":
+            failed.append({
+                "name": pod.metadata.name,
+                "namespace": pod.metadata.namespace,
+                "status": phase
+            })
+
+    return failed
