cf2c3041fbdbe80512ac598613f2343f5956b67a
prabha332 <prabhavatiagre@gmail.com>
2026-03-01 17:00:20 +0530

    Implement log collection from Kubernetes pods
    Added a function to collect logs from Kubernetes pods.

--- collectors/logs_collector.py
+++ collectors/logs_collector.py
@@ -1,1 +1,24 @@
+# Collect logs directly from Kubernetes
 
+from kubernetes import client, config
+from utils.logger import logger
+
+
+config.load_kube_config()
+v1 = client.CoreV1Api()
+
+
+def get_pod_logs(pod, namespace):
+
+    try:
+        logs = v1.read_namespaced_pod_log(
+            name=pod,
+            namespace=namespace,
+            tail_lines=100
+        )
+
+        return logs
+
+    except Exception as e:
+        logger.error(f"Log collection failed: {e}")
+        return "No logs available"
