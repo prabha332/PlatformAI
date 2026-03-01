e28f2f533c01c6d45970dd23b3db4985cc553073
prabha332 <prabhavatiagre@gmail.com>
2026-03-01 16:53:22 +0530

    Implement restart_deployment function
    Added a function to restart Kubernetes deployments.

--- remediation/auto_heal.py
+++ remediation/auto_heal.py
@@ -1,1 +1,15 @@
+#  Self-Healing Automation
+import subprocess
 
+def restart_deployment(name, namespace):
+
+    cmd = [
+        "kubectl",
+        "rollout",
+        "restart",
+        f"deployment/{name}",
+        "-n",
+        namespace
+    ]
+
+    subprocess.run(cmd)
