7dee1997937c89afff3da62e501fa95c93703f2f
prabha332 <prabhavatiagre@gmail.com>
2026-03-01 17:01:15 +0530

    Add simulated Prometheus-style metrics collector
    Implement a simulated metrics collector for pods.

--- collectors/metrics_collector.py
+++ collectors/metrics_collector.py
@@ -1,1 +1,29 @@
+# Simulated Prometheus-style metrics collector
 
+import random
+from utils.logger import logger
+
+
+def get_pod_metrics(pod, namespace):
+
+    try:
+        metrics = {
+            "cpu": random.randint(10, 95),
+            "memory": random.randint(100, 900),
+            "restarts": random.randint(0, 10)
+        }
+
+        logger.info(
+            f"Metrics collected for {pod}"
+        )
+
+        return metrics
+
+    except Exception as e:
+        logger.error(e)
+
+        return {
+            "cpu": 0,
+            "memory": 0,
+            "restarts": 0
+        }
