0c0ef3f0d9f4f9505222d90214ef0f75192bcb70
prabha332 <prabhavatiagre@gmail.com>
2026-03-01 16:50:24 +0530

    Update predictor.py

--- ai_engine/predictor.py
+++ ai_engine/predictor.py
@@ -1,1 +1,14 @@
+# Incident Prediction Model
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+import pickle
 
+data = pd.read_csv("data/incidents.csv")
+
+X = data[["cpu","memory","restarts"]]
+y = data["incident"]
+
+model = RandomForestClassifier()
+model.fit(X, y)
+
+pickle.dump(model, open("models/incident_model.pkl","wb"))
